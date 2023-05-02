4ea2c974a503c5506d6fd6eff8787b953bfa5b91
Théo Fabiano <theo.fabiano@epitech.eu>
2023-05-02 20:32:25 +0200

    ✨ feat: add middleware to check if todo exists in database

---
+++ src/middleware/notFound.js
@@ -0,0 +1,18 @@
+const db = require("../config/db");
+
+module.exports = (req, res, next) => {
+  const { id } = req.params;
+
+  if (id === undefined || parseInt(id, 10).toString() !== id) {
+    res.status(500).json({ msg: "Internal server error" });
+    return;
+  }
+
+  db.execute("SELECT * FROM `todo` WHERE id = ?", [id], (err, results) => {
+    if (results.length > 0) {
+      next();
+    } else {
+      res.status(404).json({ msg: "Not found" });
+    }
+  });
+};
